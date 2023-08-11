61dbd258484130106473163be4151fd52348950c
yuhuimin1996 <huiminyu51@gmail.com>
2023-08-11 00:10:52 -0700

    Update RecordingButton.js

--- frontend/src/components/RecordingButton.js
+++ frontend/src/components/RecordingButton.js
@@ -9,10 +9,10 @@ export default function RecordingButton({ isRecording, progress, onPress }) {
   const size = 80;
   const strokeWidth = 8;
   const radius = (size - strokeWidth) / 2;
-  const innerRadius = radius - (strokeWidth / 2); // 小圆的半径
+  const innerRadius = radius - (strokeWidth / 2);
   const circumference = 2 * Math.PI * radius;
 
-  // 初始偏移量为圆的周长，这样动画从12点钟位置开始
+  
   const dashOffset = Animated.subtract(circumference, Animated.multiply(progress, circumference));
 
   return (
